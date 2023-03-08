971531d292009b5652a5163aa093dd0b815237ea
KaKeGo <funfcia@hotmail.com>
2023-03-08 20:32:57 +0100

    first commit

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    RegisterView,
+    RetrieveUserView,
+)
+
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('me', RetrieveUserView.as_view()),
+]
+
